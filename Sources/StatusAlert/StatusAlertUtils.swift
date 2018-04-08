e03d861675bb66ac572a6d4cdf73f6a57c6f3d11
Muddassir Ahmed Khan <muddassir.ahmed235@gmail.com>
2018-04-08 14:11:15 +0500

    Update StatusAlertUtils.swift

--- Sources/StatusAlert/StatusAlertUtils.swift
+++ Sources/StatusAlert/StatusAlertUtils.swift
@@ -13,7 +13,7 @@ import UIKit
         @objc public static let common: Appearance = Appearance()
         
         /// - Note: Do not change to save system look
-        @objc public var titleFont: UIFont = UIFont.systemFont(ofSize: 23, weight: UIFontWeightSemibold)
+        @objc public var titleFont: UIFont = UIFont.systemFont(ofSize: 17, weight: UIFontWeightRegular)
         
         /// - Note: Do not change to save system look
         @objc public var messageFont: UIFont = UIFont.systemFont(ofSize: 16, weight: UIFontWeightRegular)
@@ -24,9 +24,9 @@ import UIKit
         /// Used if device does not support blur or if `Reduce Transparency` toggle
         /// in `General->Accessibility->Increase Contrast` is on
         /// - Note: Do not change to save system look
-        @objc public var backgroundColor: UIColor = UIColor.groupTableViewBackground
+        @objc public var backgroundColor: UIColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
         
-        @objc public static func copyCommon() -> Appearance {
+        static func copyCommon() -> Appearance {
             let common = Appearance.common
             let copy = Appearance()
             copy.titleFont          = common.titleFont
@@ -50,48 +50,23 @@ import UIKit
         case bottom
     }
     
-    @objc (StatusAlertSizesAndDistances)
-    public final class SizesAndDistances: NSObject {
+    enum SizesAndDistances {
+        static let defaultInitialScale: CGFloat = 0.9
+        static let defaultCornerRadius: CGFloat = 20
         
-        @objc public static let common: SizesAndDistances = SizesAndDistances()
+        static let defaultTopOffset: CGFloat = 15
+        static let defaultBottomOffset: CGFloat = 15
         
-        @objc public var defaultInitialScale: CGFloat = 0.9
-        @objc public var defaultCornerRadius: CGFloat = 10
+        static let defaultImageWidth: CGFloat = 90
+        static let defaultAlertWidth: CGFloat = 200
+        static let minimumAlertHeight: CGFloat = 200
         
-        @objc public var defaultTopOffset: CGFloat = 32
-        @objc public var defaultBottomOffset: CGFloat = 32
+        static let minimumStackViewTopSpace: CGFloat = 40
+        static let minimumStackViewBottomSpace: CGFloat = 40
+        static let stackViewSideSpace: CGFloat = 20
         
-        @objc public var defaultImageWidth: CGFloat = 90
-        @objc public var defaultAlertWidth: CGFloat = 258
-        @objc public var minimumAlertHeight: CGFloat = 240
-        
-        @objc public var minimumStackViewTopSpace: CGFloat = 44
-        @objc public var minimumStackViewBottomSpace: CGFloat = 24
-        @objc public var stackViewSideSpace: CGFloat = 24
-        
-        @objc public var defaultImageBottomSpace: CGFloat = 30
-        @objc public var defaultTitleBottomSpace: CGFloat = 5
-        @objc public var defaultImageToMessageSpace: CGFloat = 24
-        
-        @objc public static func copyCommon() -> SizesAndDistances {
-            let common = SizesAndDistances.common
-            let copy = SizesAndDistances()
-            
-            copy.defaultInitialScale            = common.defaultInitialScale
-            copy.defaultCornerRadius            = common.defaultCornerRadius
-            copy.defaultTopOffset               = common.defaultTopOffset
-            copy.defaultBottomOffset            = common.defaultBottomOffset
-            copy.defaultImageWidth              = common.defaultImageWidth
-            copy.defaultAlertWidth              = common.defaultAlertWidth
-            copy.minimumAlertHeight             = common.minimumAlertHeight
-            copy.minimumStackViewTopSpace       = common.minimumStackViewTopSpace
-            copy.minimumStackViewBottomSpace    = common.minimumStackViewBottomSpace
-            copy.stackViewSideSpace             = common.stackViewSideSpace
-            copy.defaultImageBottomSpace        = common.defaultImageBottomSpace
-            copy.defaultTitleBottomSpace        = common.defaultTitleBottomSpace
-            copy.defaultImageToMessageSpace     = common.defaultImageToMessageSpace
-            return copy
-        }
+        static let defaultImageBottomSpace: CGFloat = 10
+        static let defaultTitleBottomSpace: CGFloat = 5
     }
 }
 
@@ -99,5 +74,5 @@ import UIKit
 
 #if swift(>=4.0)
     private let UIFontWeightSemibold = UIFont.Weight.semibold
-    private let UIFontWeightRegular = UIFont.Weight.regular
+    private let UIFontWeightRegular = UIFont.Weight.bold
 #endif
